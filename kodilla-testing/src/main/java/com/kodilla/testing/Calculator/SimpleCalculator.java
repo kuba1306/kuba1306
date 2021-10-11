467ef1735d95e5145f588ff35cb4f80776f9405b
Jakub Panienski <jakubpanienski@wp.pl>
2021-10-11 15:44:21 +0200

    calculator test

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/Calculator/SimpleCalculator.java
@@ -0,0 +1,13 @@
+package com.kodilla.testing.Calculator;
+
+public class SimpleCalculator {
+
+    public int subtract(int a, int b) {
+        return  a - b;
+    }
+
+    public int add(int a, int b) {
+        return  a + b;
+    }
+
+}
--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,5 +1,6 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.Calculator.SimpleCalculator;
 import com.kodilla.testing.user.SimpleUser;
 
 public class TestingMain {
@@ -10,7 +11,25 @@ public class TestingMain {
         String result = simpleUser.getUsername();
 
         if (result.equals("theForumUser")) {
-            System.out.println("test OK, ok");
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        SimpleCalculator counter = new SimpleCalculator();
+        int a = 10;
+        int b = 5;
+        int substractResult = a - b;
+        int addResult = a + b;
+
+        if (counter.add(a, b) == addResult) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        if (counter.subtract(a, b) == substractResult) {
+            System.out.println("test OK");
         } else {
             System.out.println("Error!");
         }
