46f5983d0b0606b187a5b4bbc53b61801c277c81
Jakub Panienski <jakubpanienski@wp.pl>
2021-11-30 17:19:41 +0100

    a

---
+++ kodilla-good-pattern/src/main/java/com/kodilla/kodillagoodpattern/KodillaGoodPatternApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.kodillagoodpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaGoodPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodillaGoodPatternApplication.class, args);
+	}
+
+}
---
+++ kodilla-good-pattern/src/test/java/com/kodilla/kodillagoodpattern/KodillaGoodPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.kodillagoodpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaGoodPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
