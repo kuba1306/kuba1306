af4b9a48d97f3c12c09b75cf09ae33f452496a6d
Jakub Panienski <jakubpanienski@wp.pl>
2021-12-03 03:49:05 +0100

    kolejne

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/KodillaGoodPatternsApplication.java
@@ -0,0 +1,10 @@
+package com.kodilla.good.patterns;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaGoodPatternsApplication {
+
+    public static void main(String[] args) {
+    }
+}
