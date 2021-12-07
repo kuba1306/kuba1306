7f45a36497b28747eb5924a7afef0501443f9fbc
Jakub Panienski <jakubpanienski@wp.pl>
2021-12-07 04:45:10 +0100

    exception

--- kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -1,4 +1,17 @@
 package com.kodilla.exception.test;
 
 public class ExceptionHandling {
+
+    public static void main(String[] args) {
+        SecondChallenge secondChallenge = new SecondChallenge();
+
+        try {
+            secondChallenge.probablyIWillThrowException(2,3);
+        } catch (Exception e) {
+            System.out.println("One of those numbers contain exception");
+        }
+        finally {
+            System.out.println("Done");
+        }
+    }
 }
