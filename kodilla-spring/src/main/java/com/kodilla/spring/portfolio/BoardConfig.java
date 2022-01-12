818693616103a63e9d8ca3464821ca1594c3083e
jakubpanienski@wp.pl <jakubpanienski@wp.pl>
2022-01-12 04:35:43 +0100

    spring web

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -1,15 +1,15 @@
-package com.kodilla.spring.portfolio;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
-
-@Configuration
-public class BoardConfig {
-
-    @Bean
-    @Scope("prototype")
-
-    public TaskList getTaskList() {
-    }
-}
+//package com.kodilla.spring.portfolio;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Scope;
+//
+//@Configuration
+//public class BoardConfig {
+//
+//    @Bean
+//    @Scope("prototype")
+//
+//    public TaskList getTaskList() {
+//    }
+//}
