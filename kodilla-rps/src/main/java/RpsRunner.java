d85296191102f96e998b5fca4cbdfb2d6ec629bf
jakubpanienski@wp.pl <jakubpanienski@wp.pl>
2022-01-20 20:30:53 +0100

    rps

--- kodilla-rps/src/main/java/RpsRunner.java
+++ kodilla-rps/src/main/java/RpsRunner.java
@@ -1,17 +1,15 @@
-//import java.util.Random;
-//import java.util.Scanner;
-//
-//public class RpsRunner {
-//
-//    public static void main(String[] args) {
-//
-//        String yourNp;[
-//                4-+ame = new String();
-//        String choice = new String();
-//        Random random = new Random();
-//        Scanner scanner = new Scanner(choice);
-//
-//        System.out.println();
-//    }
-//}
+import java.util.Random;
+import java.util.Scanner;
+
+public class RpsRunner {
+
+    public static void main(String[] args) {
+
+        Display starter = new Display();
+
+        starter.displayMainMenu();
+        starter.afterMainMEnuDisplay();
+
+    }
+}
 
